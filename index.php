c7faed4f24e7a18f0fd2d4dce8e7464f334ae3e9
Your Name <you@example.com>
2022-01-21 08:49:06 +0700

    784

--- index.php
+++ index.php
@@ -2,45 +2,61 @@
 <html>
 <head>
 <title>Insert-Data</title>
+<link rel="stylesheet" href="">
 <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
 </head>
 <body>
-    <h1> Page A </h1>
-    <img id="pictureUrl" width="25%">
-  <p id="userId"></p>
-  <p id="displayName"></p>
-
-<h2> พืชเศรษฐกิจ </h2>
-<form  action = "frminsert.php" method="POST">
-    <select name="planteco" type="text" id="planteco"  required >
-        <option>พืชเศรษฐกิจอะไร</option>
-        <option value="rice">ข้าว</option>
-        <option value="rubber">ยางพารา</option>
-        <option value="sugarcane">อ้อย</option>
-        <option value="cassava">มันสำปะหลัง</option>
-        <option value="palmoil">ปาล์มน้ำมัน</option>
+<div id="login">
+<img id="pictureUrl" width="25%">
+<div class="container">
+<div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                    <form  action = "frminsert.php" method="POST" class="form">
+                    <h3 class="text-center text-info"><p id="displayName"></p></h3>
+                <div class="form-group">
+                <label for="username" class="text-info">โปรดเลือก</label><br>
+                <select name="planteco" type="text" id="planteco"  required >
+        <option class="form-control" value="rice">ข้าว</option>
+        <option class="form-control" value="rubber">ยางพารา</option>
+        <option class="form-control" value="sugarcane">อ้อย</option>
+        <option class="form-control" value="cassava">มันสำปะหลัง</option>
+        <option class="form-control" value="palmoil">ปาล์มน้ำมัน</option>
     </select><br>
-
-<select name="epi" type="text" id="epi"  required >
-        <option>เกี่ยวกับอะไร</option>
-        <option value="epidemic">โรคระบาด</option>
-        <option value="pest">ศัตรูพืช</option>
+    </div>
+    <div class="form-group">
+    <label for="password" class="text-info">โปรดเลือก</label><br>
+    <select name="epi" type="text" id="epi"  required >
+        <option class="form-control" value="epidemic">โรคระบาด</option>
+        <option class="form-control" value="pest">ศัตรูพืช</option>
     </select><br>
+    <div class="form-group">
+<label  for="username" class="text-info" >รายละเอียด</label><br>
+<input name="descrip" type="text" id="descrip" class="form-control"   required> <br>
+</div>
 
-<label>รายละเอียด</label><br>
-<input name="descrip" type="text" id="descrip"   required> <br>
-
-<input type="submit" value="Save">
-<input type="reset" value="Cancel">
-
+<div class="form-group">
+<input class="btn btn-info btn-md" type="submit" value="Save">
+<input class="btn btn-danger btn-md" type="reset" value="Cancel">
+</div>
 </form>
-                          
-<!-- ไลน์ liff -->
+</div>
+                </div>
+            </div>
+        </div>
+    </div>                  
+
+
+    <!-- ไลน์ liff -->
 <script>
     function runApp() {
       liff.getProfile().then(profile => {
         document.getElementById("pictureUrl").src = profile.pictureUrl;
-        document.getElementById("userId").innerHTML = '<b>UserId:</b> ' + profile.userId;
         document.getElementById("displayName").innerHTML = '<b>DisplayName:</b> ' + profile.displayName;
       }).catch(err => console.error(err));
     }
