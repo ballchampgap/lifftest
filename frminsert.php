dc694d1c71ad833fc7919a8cc79d729317306931
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-01-26 00:14:00 +0700

    etfe525

--- frminsert.php
+++ frminsert.php
@@ -10,16 +10,16 @@ $epi = $_POST['epi'];
 $pest = $_POST['pest'];
 $descrip = $_POST['descrip'];
 
-if ($epi=='epidemic'){
+// if ($epi=='epidemic'){
 
     $sql =  "INSERT INTO epis (lat,lon,plant,pest_epis,what,descrip,yourname)VALUES('$lat', '$lon','$planteco', '$epi','$descrip','$pname')";
     $myPOD->query($sql);
-}
-else{
+// }
+// else{
 
-    $sql =  "INSERT INTO datapess (lat,lon,plant,pest_epis,what,descrip,yourname)VALUES('$lat', '$lon','$planteco', '$epi','$descrip','$pname')";
-    $myPOD->query($sql);
-}
+//     $sql =  "INSERT INTO datapess (lat,lon,plant,pest_epis,what,descrip,yourname)VALUES('$lat', '$lon','$planteco', '$epi','$descrip','$pname')";
+//     $myPOD->query($sql);
+// }
 //บันทึกสำเร็จแจ้งเตือนและกระโดดกลับไปหน้าฟอร์ม   
 if ($myPOD) 
 {
