84003d3dae3684c8af5a66e30ac7cbcbb989702a
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-01-28 19:36:09 +0700

    etfe525454[

--- connect.php
+++ connect.php
@@ -1,7 +1,22 @@
 <?php
-$host        = "host = ec2-54-157-15-228.compute-1.amazonaws.com";
-   $port        = "port = 5432";
-   $dbname      = "dbname = d7fj264o5io37t";
-   $credentials = "user = hnkkpwkeaplrup password=815f51a88490c034b86254dc776c49d16d9c20e4858ffc186e73ff74072aa98b";
-   $db = pg_connect( "$host $port $dbname $credentials");
-?>
+
+$host = "ec2-54-157-15-228.compute-1.amazonaws.com";
+$user = "hnkkpwkeaplrup";
+$password = "815f51a88490c034b86254dc776c49d16d9c20e4858ffc186e73ff74072aa98b";
+$dbname = "d7fj264o5io37t";
+$port = "5432";
+
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+
+  //create a pdo instance
+  $conn = new PDO($dsn, $user, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connection succesfully";
+}
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
+  ?>
