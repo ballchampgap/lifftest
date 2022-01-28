e00fbe32ad41bae79d3bf5666e7e2145f97c6c9e
ballchampgap <98090051+ballchampgap@users.noreply.github.com>
2022-01-28 20:07:03 +0700

    etfe525454[-

--- frminsert.php
+++ frminsert.php
@@ -1,7 +1,7 @@
 <?php
-include_once 'connect.php';
-if(isset($_POST['save']))
-{
+
+session_start();
+require_once "connect.php";
 
 $pname = $_POST['displayName'];
 $lat = $_POST['lat'];
@@ -12,15 +12,23 @@ $pest = $_POST['pest'];
 $descrip = $_POST['descrip'];
 
 // if ($epi=='epidemic'){
-    $sql ="INSERT INTO epis(lat,lon,plant,pest_epis,what,descrip,yourname)
-    values('$lat', '$lon','$planteco', '$epi','$descrip','$pname')";
-     if($result = pg_query($sql)){
-		echo "Data Added Successfully.";
-	 }
-	 else{
-		echo "Error.";
-	 }
-}
+    $sql = $conn->prepare("INSERT INTO epis(lat,lon,plant,pest_epis,what,descrip,yourname)VALUES(:lat, :lon, :planteco, :epi, :pest, :descrip :pname)");
+    $sql->bindParam(":lat", $lat);
+	$sql->bindParam(":lon", $lon);
+	$sql->bindParam(":planteco", $planteco);
+	$sql->bindParam(":epi", $epi);
+	$sql->bindParam(":pest", $pest);
+	$sql->bindParam(":descrip", $descrip);
+	$sql->bindParam(":pname", $pname);
+	$sql->execute();
+
+	if ($sql){
+		$_SESSION['success'] = "Data has been inserted succesfully";
+		header("location: index.php");
+	}else{
+		$_SESSION['error'] = "Data has been inserted succesfully";
+		header("location: index.php");
+	}
 // }
 // else{
 
